78ac4171956bd7b9fed9a43b5b218aafb0bdea9d
c-tasnia <tcpuc1042@gmail.com>
2026-06-14 18:43:55 +0600

    Update query.sql

--- query.sql
+++ query.sql
@@ -85,3 +85,19 @@ FROM bookings
 WHERE payment_status IS NULL;
 
 
+
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+
+
+SELECT
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM bookings b
+INNER JOIN users   u ON b.user_id  = u.user_id
+INNER JOIN matches m ON b.match_id = m.match_id
+ORDER BY b.booking_id;
+
+
+
