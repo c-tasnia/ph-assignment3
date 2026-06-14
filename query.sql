b7bad5cd8e8fdcffa1b8cd3beb00316b7b03f11a
c-tasnia <tcpuc1042@gmail.com>
2026-06-14 18:46:24 +0600

    Update query.sql

--- query.sql
+++ query.sql
@@ -129,4 +129,15 @@ WHERE total_cost > (
 ORDER BY booking_id;
 
 
+-- Query 7: RRetrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+
+
+SELECT
+    match_id,
+    fixture,
+    base_ticket_price
+FROM matches
+ORDER BY base_ticket_price DESC
+LIMIT 2 OFFSET 1;
+
 
